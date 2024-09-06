d390d0f5526ee0e3f854c1f7fb80926215e5bf52
sarkarranjanpartha <sarkarranjanpartha@gmail.com>
2024-09-06 23:51:33 +0530

    Calling of useState changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import EmployeesCollapsibleTable from "./EmployeeCollapsibleTable";
 import DepartmentCollapsibleTable from "./DepartmentCollapsibleTable";
-import { Divider } from "@mui/material";
+// import EmployeesCollapsibleTable from "./EmployeeCollapsibleTable";
+// import { Divider } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <EmployeesCollapsibleTable />
-      <Divider />
+      {/* <EmployeesCollapsibleTable />
+      <Divider /> */}
       <DepartmentCollapsibleTable />
     </div>
   );
