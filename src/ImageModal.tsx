836d0b1c8639a04ea032f5ca6dd0d1a8486f7a81
sarkarranjanpartha <sarkarranjanpartha@gmail.com>
2024-09-10 19:06:35 +0530

    ValidateDOMNesting error fixed using cursor ai

--- src/ImageModal.tsx
+++ src/ImageModal.tsx
@@ -1,37 +1,61 @@
-import { Dialog } from "@mui/material";
+import { Typography } from "@mui/material";
 import React from "react";
 
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+
 export interface ImageModalProps {
+  open: boolean;
+  onClose: () => void;
   mediaUrl: string;
   mediaTitle: string;
-  openImageModal: boolean;
-  setOpenImageModal: (pram: boolean) => void;
 }
 
-const ImageModal = (props: ImageModalProps) => {
-  console.log(props);
-  const { mediaUrl, mediaTitle, openImageModal } = props;
-  const handleCloseImageModal = () => props.setOpenImageModal(false);
-
+const ImageModal: React.FC<ImageModalProps> = ({
+  open,
+  onClose,
+  mediaUrl,
+  mediaTitle,
+}) => {
   return (
-    <div>
-      <React.Fragment>
-        <Dialog
-          fullWidth={false}
-          maxWidth={"lg"}
-          open={openImageModal}
-          onClose={handleCloseImageModal}
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-title"
+      aria-describedby="modal-description"
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          bgcolor: "background.paper",
+          border: "2px solid #000",
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        <Typography
+          id="image-modal-title"
+          variant="h6"
+          component="h2"
+          gutterBottom
         >
-          <img
-            src={mediaUrl}
-            alt={mediaTitle}
-            loading="lazy"
-            width="300px"
-            height="300px"
-          />
-        </Dialog>
-      </React.Fragment>
-    </div>
+          {mediaTitle}
+        </Typography>
+        <img
+          src={mediaUrl}
+          alt="Media preview"
+          style={{
+            maxWidth: "100%",
+            maxHeight: "calc(90vh - 100px)",
+            objectFit: "contain",
+          }}
+        />
+      </Box>
+    </Modal>
   );
 };
 export default ImageModal;
