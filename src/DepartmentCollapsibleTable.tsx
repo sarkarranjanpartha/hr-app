7d0e8375aa29cd5566f130e3e50a650652de827e
Partha Sarkar <sarkarranjanpartha@gmail.com>
2024-09-20 01:12:21 +0530

    Feature/refrash after insert update (#4)
    * Removed commented code
    
    * Refrashed after update
    
    * Refrashed after insert
    
    * Refrashed after insert update

--- src/DepartmentCollapsibleTable.tsx
+++ src/DepartmentCollapsibleTable.tsx
@@ -14,12 +14,6 @@ const DepartmentCollapsibleTable = () => {
   const [empsData, setEmpsData] = useState<EmployeeProps[]>([]);
   const [deptsData, setDeptsData] = useState<DepartmentProps[]>([]);
   const [dataChanged, setDataChanged] = useState(false);
-  const [openConfirmationDepartmentModal, setOpenConfirmationDepartmentModal] =
-    React.useState<boolean>(false);
-  const handleCloseConfirmationDepartmentModal = () => {
-    setOpenConfirmationDepartmentModal(false);
-    callRefreshFunction();
-  };
 
   const departmentsData = () => {
     const getAllDepartmentArgs: apiParamsProps = {
@@ -77,6 +71,7 @@ const DepartmentCollapsibleTable = () => {
       empsData,
       deptsData.sort((a, b) => a.department_id - b.department_id)
     );
+
   return (
     <>
       <div>Department Collapsible Table</div>
